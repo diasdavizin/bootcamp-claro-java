d1e5c84e859fc3211e3fda04d1a75b12db37115d
diasdavizin <davizin042005@gmail.com>
2024-08-24 15:47:02 -0300

    feat: making calls to candidates

--- desafios/src/ControleCandidatos/ProcessoSeletivo.java
+++ desafios/src/ControleCandidatos/ProcessoSeletivo.java
@@ -1,10 +1,39 @@
 package ControleCandidatos;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        String [] candidatos = { "FELIPE", "MARCIA", "JOAO", "CARLOS", "LUCAS"};
+        for( String candidato: candidatos){
+            System.out.println("O candidato é o: " + candidato);
+            entrandoEmContato(candidato);
+        }
     }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if ( continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+        }while  (continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + " TENTATIVA REALIZADA. " );
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " NÚMERO MAXIMO DE " + tentativasRealizadas + " TENTATIVAS REALIZADA." );
+    }
+
+    // método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
     static void imprimirSelecionados(){
         String [] candidatos = { "FELIPE", "MARCIA", "JOAO", "CARLOS", "LUCAS"};
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento.");
